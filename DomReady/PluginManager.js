86e21e92a2eb95df21537bef56c9fa7fb15c4378
stupid cat <kirkbenm@gmail.com>
2017-07-06 10:35:50 -0600

    plugin changes

--- DomReady/PluginManager.js
+++ DomReady/PluginManager.js
@@ -10,53 +10,67 @@ class PluginManager {
         this.classes = {};
         this.plugins = {};
 
-        this.walk(window._path.join(__dirname, '..', 'Plugins'));
-    }
-
-    walk(directory) {
-        window._fs.readdir(directory, (err, files) => {
+        window._fs.readdir(this.constructPath(), (err, files) => {
             for (const file of files) {
-                switch (window._path.extname(file)) {
-                    case '.js':
-                        this.load(window._path.join(directory, file));
-                        break;
-                    case '':
-                        this.walk(window._path.join(directory, file));
-                        break;
+                try {
+                    if (window._fs.lstatSync(this.constructPath(file)).isDirectory()) {
+                        this.load(file);
+                    }
+                } catch (err) {
+                    console.error('Failed to load plugin', file, err);
                 }
             }
         });
     }
 
-    load(file) {
-        const Plugin = require(file);
-        if (Plugin && Plugin.constructor) {
-            if (Plugin.prototype instanceof PluginStruct && Plugin.prototype.constructor.name === window._path.basename(file, '.js')) {
-                const plugin = new Plugin();
-                plugin.filePath = file;
-                const name = plugin.name;
+    load(name) {
+        try {
+            let pack;
+            try {
+                pack = reload(this.constructPath(name, 'package'));
+            } catch (err) { }
+            if (!(pack instanceof Object)) {
+                console.error(`Plugin '${name}' had an invalid package file, skipping`);
+                return;
+            }
+            let entryPoint = pack.main || 'index';
+            const Plugin = reload(this.constructPath(name, entryPoint));
+            if (Plugin && Plugin.constructor && Plugin.prototype instanceof PluginStruct) {
+                const plugin = new Plugin(this.constructPath(name));
                 this.classes[name] = Plugin;
                 this.plugins[name] = plugin;
                 plugin.log('Loaded!');
+            } else {
+                console.error(`Plugin '${name}''s entry point was not an instance of the Plugin structure, skipping`);
             }
+        } catch (err) {
+            console.error('Failed to load plugin', name, err);
         }
     }
 
     unload(name) {
         this.plugins[name].unload();
         delete this.plugins[name];
+        console.log(`Plugin ${name} has been unloaded.`);
     }
 
     reload(name) {
-        const file = this.plugins[name].filePath;
-        console.log(file);
-        this.plugins[name].unload();
-        delete this.plugins[name];
-        this.classes[name] = reload(file);
-        if (this.classes[name].prototype instanceof PluginStruct) {
-            this.plugins[name] = new this.classes[name]();
-            this.plugins[name].filePath = file;
+        try {
+            const file = this.plugins[name].filePath;
+            this.unload(name);
+            this.load(name);
+            console.log(`Plugin ${name} has been reloaded.`);
+        } catch (err) {
+            console.error('Failed to reload plugin', window._path.basename(name));
         }
+    }
+
+    constructPath(...args) {
+        return this.path.join(__dirname, '..', 'Plugins', ...args);
+    }
+
+    get path() {
+        return window._path;
     }
 }
 
--- Structures/Plugin.js
+++ Structures/Plugin.js
@@ -1,21 +1,77 @@
 class Plugin {
     /**
      * Plugin constructor
-     * @param {Object} info - The info object
-     * @param {String} [info.name] - The name of the plugin, defaults to constructor name
-     * @param {String} info.author - The author of the plugin
-     * @param {String} info.version - The version of the plugin
-     * @param {String} info.description - A description of the plugin
-     * @param {String} [info.color] - An optional log colour
+     * @param {Object} pack - The package.json object
+     * @param {Object} config - The config object
+     * @param {String} [config.color] - An optional log colour
+     * @param {Any} [...] - Any other config fields
      */
-    constructor(info) {
+    constructor(path) {
         if (this.constructor == Plugin) {
             throw new Error('Cannot instantiate an abstract class!');
         }
-        if (!info.hasOwnProperty('author') || !info.hasOwnProperty('version') || !info.hasOwnProperty('description')) {
+        this._load(path);
+    }
+
+    get configTemplate() {
+        return {
+            color: 0x444444
+        };
+    }
+
+    _load(path) {
+        this.path = path;
+        this._loadPackage();
+        this._loadConfig();
+        this._loadCss();
+    }
+
+    _loadPackage(pack) {
+        this.pack = require(window._path.join(this.path, 'package'));
+        if (!this.pack.hasOwnProperty('author') || !this.pack.hasOwnProperty('version') || !this.pack.hasOwnProperty('description')) {
             throw new Error('A plugin must have an author, version, and description');
         }
-        this.info = info;
+    }
+
+    _loadConfig() {
+        try {
+            this.config = require(window._path.join(this.path, 'config'));
+        } catch (err) {
+            let config = this.configTemplate;
+            window._fs.writeFile(window._path.join(this.path, 'config.json'), JSON.stringify(config, null, 2));
+            this.config = config;
+        }
+    }
+
+    _loadCss() {
+        let cssElement = this._styleTag;
+        if (!cssElement) {
+            cssElement = document.getElementById(`CSS-${this.name}`) || document.createElement('style');
+            cssElement.id = `CSS-${this.name}`;
+            this._styleTag = cssElement;
+            document.head.appendChild(this._styleTag);
+        }
+        let cssDir = window._path.join(this.path, 'css');
+        if (window._fs.lstatSync(cssDir).isDirectory()) {
+            let files = window._fs.readdirSync(cssDir);
+            let css = '';
+            for (const file of files) {
+                if (window._path.extname(file) !== '.css') continue;
+                css += `/* ${file} */\n\n`;
+                let content = window._fs.readFileSync(window._path.join(cssDir, file), { encoding: 'UTF8' });
+                css += content + '\n\n';
+            }
+            cssElement.innerHTML = css;
+        }
+
+        if (!this._cssWatcher) {
+            this._cssWatcher = window._fs.watch(this.path, { encoding: 'utf-8' },
+                eventType => {
+                    if (eventType == 'change') {
+                        this._loadCss();
+                    }
+                });
+        }
     }
 
     /**
@@ -26,27 +82,31 @@ class Plugin {
     }
 
     get name() {
-        return this.info.name || this.constructor.name;
+        return this.pack ? this.pack.name || this.constructor.name : this.constructor.name;
     }
 
     get author() {
-        return this.info.author;
+        return this.pack.author;
     }
 
     get version() {
-        return this.info.version;
+        return this.pack.version;
     }
 
     get description() {
-        return this.info.description;
+        return this.pack.description;
     }
 
     get color() {
-        return this.info.color || 0x444444;
+        return this.config ? this.config.color || 0x444444 : 0x444444;
     }
 
     log(...args) {
         console.log(`%c[${this.name}]`, `color: #${this.color}; font-weight: bold;`, ...args);
+    }
+
+    error(...args) {
+        console.error(`%c[${this.name}]`, `color: #${this.color}; font-weight: bold;`, ...args);
     }
 }
 
