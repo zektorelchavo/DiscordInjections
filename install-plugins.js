630100ab4d318d3964e344cb4566394037a5bb71
stupid cat <cat@blargbot.xyz>
2017-07-10 20:57:58 -0600

    Fixed an install bug

--- install-plugins.js
+++ install-plugins.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
+const Promise = require('bluebird');
 
 const files = fs.readdirSync(path.join(__dirname, 'Plugins'));
-let promise;
-for (let name of files) {
-    if (!promise) promise = npmInstall(name);
-    else promise.then(() => npmInstall(name));
-}
-promise.then(() => {
+
+Promise.each(files, npmInstall).then(() => {
     console.log('Done!');
     process.exit();
     return;
@@ -19,33 +16,17 @@ function npmInstall(name) {
         console.log('*=== Installing:', name, '===*');
         const file = path.join(__dirname, 'Plugins', name);
         if (fs.lstatSync(file).isDirectory()) {
-            let stdout = '', stderr = '';
-            const child = childProcess.exec('npm install', {
+            childProcess.exec('npm install', {
                 cwd: file,
                 encoding: 'utf8'
             }, (err, sout, serr) => {
                 if (err) console.error(err);
-                if (stdout) console.log(stdout);
-                if (stderr) console.error(stderr);
-                child.removeAllListeners();
+                if (sout) console.log(sout);
+                if (serr) console.error(serr);
                 res();
             });
-
-            child.stdout.on('data', chunk => {
-                stdout += chunk;
-            });
-            child.stdout.on('end', () => {
-                console.log(stdout);
-                stdout = '';
-            });
-
-            child.stderr.on('data', chunk => {
-                stderr += chunk;
-            });
-            child.stderr.on('end', () => {
-                console.error(stderr);
-                stderr = '';
-            });
+        } else {
+            console.log('File ' + name + ' was not a directory.');
         }
     });
 }
