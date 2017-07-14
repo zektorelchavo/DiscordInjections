09ff4e4a5c9c59dc7234154558d6eea62891f446
stupid cat <cat@blargbot.xyz>
2017-07-13 20:55:00 -0600

    no more invalid command becuase it succs

--- DomReady/CommandHandler.js
+++ DomReady/CommandHandler.js
@@ -269,19 +269,16 @@ class CommandHandler {
                     if (event.shiftKey) return;
                     let command = this.textarea.value;
                     command = command.substring(this.prefix.length).trim();
-                    this.textarea.value = '';
-                    this.onInput();
-                    event.preventDefault();
-
+                    
                     let [name, ...args] = command.split(' ');
                     name = name.toLowerCase();
                     if (this.commands[name]) {
+                        this.textarea.value = '';
+                        this.onInput();
+                        event.preventDefault();
                         let output = this.commands[name]._execute(args);
                         if (output)
                             window.DI.client.selectedChannel.send(output);
-                    } else {
-                        window.DI.Helpers.sendDI(`A command with the name <code class='inline'>${name}</code> could not be found. If you meant to send a regular message, please insert a space in front of your content.
-                        <br>For reference, the command you tried to execute was:<br><pre><code class='hljs'>${command}</code></pre>`, false);
                     }
                     break;
             }
