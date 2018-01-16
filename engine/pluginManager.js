6e3910570bea03abc9dbee69b441978574a13c04
Christopher König <criss@paars.de>
2018-01-16 12:39:22 +0100

    standardjs reformat

--- engine/pluginManager.js
+++ engine/pluginManager.js
@@ -1,49 +1,49 @@
-const { EventEmitter } = require('eventemitter3')
-const fs = require('fs-extra')
-const path = require('path')
-const reload = require('require-reload')
-const Promise = require('bluebird')
+const { EventEmitter } = require("eventemitter3")
+const fs = require("fs-extra")
+const path = require("path")
+const reload = require("require-reload")
+const Promise = require("bluebird")
 
 class PluginManager extends EventEmitter {
-  constructor (DI) {
+  constructor(DI) {
     super()
     this.DI = DI
     this._ready = false
     this.plugins = {}
 
     this.pluginsEnabled = {}
-    if (DI.localStorage['DI-Plugins'] != '') {
+    if (DI.localStorage["DI-Plugins"] != "") {
       try {
-        this.pluginsEnabled = JSON.parse(DI.localStorage['DI-Plugins'])
+        this.pluginsEnabled = JSON.parse(DI.localStorage["DI-Plugins"])
       } catch (ex) {}
     }
 
-    this.basePath = this.expand(DI.conf.pluginPath || './Plugins')
+    this.basePath = this.expand(DI.conf.pluginPath || "./Plugins")
     fs.ensureDirSync(this.basePath)
 
     const dependencies = []
   }
 
-  expand (basePath) {
+  expand(basePath) {
     const home =
       process.env.HOME ||
       process.env.USERPROFILE ||
       process.env.HOMEDRIVE + process.env.HOMEPATH
     const appData = process.env.HOME
-      ? path.join(process.env.HOME, '.config', 'DiscordInjections')
-      : path.join(process.env.APPDATA, 'DiscordInjections')
-    const discordPath = path.join(process.resourcesPath, '..', '..')
+      ? path.join(process.env.HOME, ".config", "DiscordInjections")
+      : path.join(process.env.APPDATA, "DiscordInjections")
+    const discordPath = path.join(process.resourcesPath, "..", "..")
 
     fs.ensureDirSync(appData)
     return basePath
-      .replace(/\\/g, '/')
-      .replace(/^\.\//, path.join(__dirname, '..') + '/')
-      .replace(/^~\//, home + '/')
+      .replace(/\\/g, "/")
+      .replace(/^\.\//, path.join(__dirname, "..") + "/")
+      .replace(/^~\//, home + "/")
       .replace(/^%\//, appData)
       .replace(/^\&\//, discordPath)
   }
 
-  loadPluginPath () {
+  loadPluginPath() {
     // look through the plugin directory
     return (
       Promise.resolve(fs.readdir(this.basePath))
@@ -52,19 +52,17 @@ class PluginManager extends EventEmitter {
         // filter out non directories
         .filter(f => fs.statSync(f).isDirectory())
         // filter out non node modules
-        .filter(f => fs.existsSync(path.join(f, 'package.json')))
-
+        .filter(f => fs.existsSync(path.join(f, "package.json")))
         // load dem plugins
         .each(m => {
           return this.load(m, false)
         })
-
-        .then(this.emit('plugins-preloaded', Object.keys(this.plugins)))
+        .then(this.emit("plugins-preloaded", Object.keys(this.plugins)))
     )
   }
 
-  async loadByPath (pluginPath, force = true) {
-    const pkg = reload(path.join(pluginPath, 'package.json'))
+  async loadByPath(pluginPath, force = true) {
+    const pkg = reload(path.join(pluginPath, "package.json"))
     if (!force && this.pluginsEnabled[pkg.name] === false) {
       // dont load unenabled plugins
       return
@@ -76,7 +74,7 @@ class PluginManager extends EventEmitter {
     }
 
     if (this.plugins[pkg.name] && this.plugins[pkg.name].loading) {
-      throw new Error('circular dependency found, aborting!')
+      throw new Error("circular dependency found, aborting!")
     }
 
     const p = (this.plugins[pkg.name] = {
@@ -94,7 +92,7 @@ class PluginManager extends EventEmitter {
     }
 
     // load the plugin
-    p.cls = reload(path.join(pluginPath, pkg.main || 'index'))
+    p.cls = reload(path.join(pluginPath, pkg.main || "index"))
     p.inst = new p.cls(this, p)
     p.loaded = true
     p.loading = false
@@ -107,16 +105,16 @@ class PluginManager extends EventEmitter {
     }
   }
 
-  async load (plugin, force = true) {
+  async load(plugin, force = true) {
     const pluginPath = path.resolve(this.basePath, plugin)
-    if (!fs.existsSync(path.join(pluginPath, 'package.json'))) {
-      throw new Error('plugin not found', plugin)
+    if (!fs.existsSync(path.join(pluginPath, "package.json"))) {
+      throw new Error("plugin not found", plugin)
     }
 
     return this.loadByPath(pluginPath, force)
   }
 
-  async unload (name) {
+  async unload(name) {
     if (!this.plugins[name] || !this.plugins[name].loaded) {
       return true
     }
@@ -133,7 +131,7 @@ class PluginManager extends EventEmitter {
     return true
   }
 
-  async reload (name, recursive = false) {
+  async reload(name, recursive = false) {
     if (!this.plugins[name]) {
       return this.load(name, true)
     }
@@ -149,10 +147,10 @@ class PluginManager extends EventEmitter {
     return await this.load(name)
   }
 
-  async enable (name, load = false) {
+  async enable(name, load = false) {
     const pluginPath = path.resolve(this.basePath, plugin)
-    if (!fs.existsSync(path.join(pluginPath, 'package.json'))) {
-      throw new Error('plugin not found', plugin)
+    if (!fs.existsSync(path.join(pluginPath, "package.json"))) {
+      throw new Error("plugin not found", plugin)
     }
 
     this.pluginsEnabled[name] = true
@@ -163,10 +161,10 @@ class PluginManager extends EventEmitter {
     return true
   }
 
-  async disable (name, unload = false) {
+  async disable(name, unload = false) {
     const pluginPath = path.resolve(this.basePath, plugin)
-    if (!fs.existsSync(path.join(pluginPath, 'package.json'))) {
-      throw new Error('plugin not found', plugin)
+    if (!fs.existsSync(path.join(pluginPath, "package.json"))) {
+      throw new Error("plugin not found", plugin)
     }
 
     this.pluginsEnabled[name] = false
@@ -177,7 +175,7 @@ class PluginManager extends EventEmitter {
     return true
   }
 
-  ready () {
+  ready() {
     if (this._ready) {
       return
     }
@@ -193,7 +191,7 @@ class PluginManager extends EventEmitter {
     ).then(() => this.emit("plugins-loaded", Object.keys(this.plugins)))
   }
 
-  get (name, raw = false) {
+  get(name, raw = false) {
     if (!this.plugins || !this.plugins[name] || !this.plugins[name].loaded) {
       return null
     }
