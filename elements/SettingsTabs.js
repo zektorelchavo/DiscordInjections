0c679520418e88bd8114201c45782f4240bcb586
Christopher König <criss@paars.de>
2018-01-17 12:28:26 +0100

    A simple tab react component

---
+++ elements/SettingsTabs.js
@@ -0,0 +1,43 @@
+const e = window.React.createElement
+
+module.exports = class SettingsTabs extends React.PureComponent {
+  constructor() {
+    super()
+    this.props = this.props || {}
+    this.state = {
+      activeIndex: this.props.activeIndex || 0
+    }
+  }
+
+  render() {
+    const tabNodes = this.props.children.map((child, index) => {
+      return e(
+        "li",
+        {
+          onClick: () => this._handleClick(index),
+          className: this.state.activeIndex === index ? "active" : ""
+        },
+        child.props.display
+      )
+    })
+
+    const contentNodes = e(
+      "div",
+      { className: "di-tabs-area" },
+      this.props.children[this.state.activeIndex]
+    )
+
+    return e(
+      "div",
+      { className: "di-tabs" },
+      e("ul", { className: "item-2zi_5J marginBottom8-1mABJ4 horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ cardPrimaryEditable-2IQ7-V card-3DrRmC di-tabs-tab" }, tabNodes),
+      e("section", {}, contentNodes)
+    )
+  }
+
+  _handleClick(index) {
+    this.setState({
+      activeIndex: index
+    })
+  }
+}
--- elements/index.js
+++ elements/index.js
@@ -1,51 +1,14 @@
-const SettingsDescription = require("./SettingsDescription")
-const SettingsDivider = require("./SettingsDivider")
-const SettingsOption = require("./SettingsOption")
-const SettingsOptionTextbox = require("./SettingsOptionTextbox")
-const SettingsOptionFilebox = require("./SettingsOptionFilebox")
-const SettingsOptionToggle = require("./SettingsOptionToggle")
-const SettingsOptionTitle = require("./SettingsOptionTitle")
-const SettingsOptionDescription = require("./SettingsOptionDescription")
-const SettingsOptionButton = require("./SettingsOptionButton")
-const SettingsExpandableSection = require("./SettingsExpandableSection")
-const SettingsSection = require("./SettingsSection")
-const SettingsTitle = require("./SettingsTitle")
-
-module.exports = {
-  get SettingsDescription() {
-    return SettingsDescription
-  },
-  get SettingsDivider() {
-    return SettingsDivider
-  },
-  get SettingsOption() {
-    return SettingsOption
-  },
-  get SettingsOptionTextbox() {
-    return SettingsOptionTextbox
-  },
-  get SettingsOptionFilebox() {
-    return SettingsOptionFilebox
-  },
-  get SettingsOptionToggle() {
-    return SettingsOptionToggle
-  },
-  get SettingsOptionTitle() {
-    return SettingsOptionTitle
-  },
-  get SettingsOptionButton() {
-    return SettingsOptionButton
-  },
-  get SettingsOptionDescription() {
-    return SettingsOptionDescription
-  },
-  get SettingsExpandableSection() {
-    return SettingsExpandableSection
-  },
-  get SettingsSection() {
-    return SettingsSection
-  },
-  get SettingsTitle() {
-    return SettingsTitle
-  }
-}
+exports.SettingsDescription = require("./SettingsDescription")
+exports.SettingsDivider = require("./SettingsDivider")
+exports.SettingsOption = require("./SettingsOption")
+exports.SettingsOptionTextbox = require("./SettingsOptionTextbox")
+exports.SettingsOptionFilebox = require("./SettingsOptionFilebox")
+exports.SettingsOptionToggle = require("./SettingsOptionToggle")
+exports.SettingsOptionTitle = require("./SettingsOptionTitle")
+exports.SettingsOptionDescription = require("./SettingsOptionDescription")
+exports.SettingsOptionButton = require("./SettingsOptionButton")
+exports.SettingsExpandableSection = require("./SettingsExpandableSection")
+exports.SettingsSection = require("./SettingsSection")
+exports.SettingsTitle = require("./SettingsTitle")
+exports.SettingsTabs = require("./SettingsTabs")
+exports.SettingsTabsPane = require("./SettingsTabsPane")
