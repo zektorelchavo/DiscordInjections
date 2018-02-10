14f0a802600b3f19fe9818c3c9897cae11a6a0a9
Ch. König <criss@paars.de>
2018-02-10 10:10:45 +0100

    adjusted the engine loader to the new plugin system

--- engine/index.js
+++ engine/index.js
@@ -1,57 +1,77 @@
-const { getCurrentWindow, getCurrentWebContents } = require("electron").remote
-const path = require("path")
-const fs = require("fs")
-const Promise = require("bluebird")
+const { getCurrentWindow, getCurrentWebContents } = require('electron').remote
+const path = require('path')
+const fs = require('fs')
+const Promise = require('bluebird')
 
 // stage one
 // prelaunch adjustments
 const DI = {
-  get conf() {
-    const conf = fs.existsSync(path.join(__dirname, "..", "config.json"))
-      ? require(path.join(__dirname, "..", "config.json"))
+  get conf () {
+    const conf = fs.existsSync(path.join(__dirname, '..', 'config.json'))
+      ? require(path.join(__dirname, '..', 'config.json'))
       : {}
     return conf
   },
 
-  get package() {
-    return require("../package.json")
+  get package () {
+    return require('../package.json')
   },
 
-  get version() {
+  get version () {
     return this.package.version
   }
 }
 
-Object.defineProperty(DI, "localStorage", {
+Object.defineProperty(DI, 'localStorage', {
   writable: false,
-  value: require("./localStorage")
+  value: require('./localStorage')
 })
 
-Object.defineProperty(DI, "client", {
+Object.defineProperty(DI, 'client', {
   writable: false,
-  value: (require("./client"))(DI)
+  value: require('./client')(DI)
 })
 
-Object.defineProperty(DI, "plugins", {
+Object.defineProperty(DI, 'plugins', {
   writable: false,
-  value: new (require("./pluginManager"))(DI)
+  value: new (require('./pluginManager'))(DI)
 })
 
 // stage two
 // post launch patching
-process.once("loaded", async () => {
-  window.include = (file, ...args) => {
-    return require(path.resolve(__dirname, "..", file), ...args)
-  }
-
-  const ready = new Promise(rs => getCurrentWebContents().on("dom-ready", rs))
+process.once('loaded', async () => {
+  window.DI = DI
+  const ready = new Promise(rs => getCurrentWebContents().on('dom-ready', rs))
 
   // add core modules
-  await DI.plugins.loadByPath(path.join(__dirname, "plugins", "react"), true)
-  await DI.plugins.loadByPath(path.join(__dirname, "plugins", "settings"), true)
-  await DI.plugins.loadByPath(path.join(__dirname, "plugins", "commands"), true)
-  await DI.plugins.loadByPath(path.join(__dirname, "plugins", "changelog"))
-  await DI.plugins.loadByPath(path.join(__dirname, "plugins", "css"), true)
+  await DI.plugins.loadByPath(
+    path.join(__dirname, 'plugins', 'react'),
+    true,
+    true
+  )
+  await DI.plugins.loadByPath(
+    path.join(__dirname, 'plugins', 'settings'),
+    true,
+    true
+  )
+  await DI.plugins.loadByPath(
+    path.join(__dirname, 'plugins', 'plugins'),
+    true,
+    true
+  )
+  await DI.plugins.loadByPath(
+    path.join(__dirname, 'plugins', 'commands'),
+    true,
+    true
+  )
+  await DI.plugins.loadByPath(path.join(__dirname, 'plugins', 'changelog'))
+  DI.plugins.get('changelog', true).core = true
+
+  await DI.plugins.loadByPath(
+    path.join(__dirname, 'plugins', 'css'),
+    true,
+    true
+  )
 
   // add external modules
   await DI.plugins.loadPluginPath()
