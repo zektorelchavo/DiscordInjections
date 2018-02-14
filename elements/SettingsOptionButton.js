951357ccf42e69e6183f9d5e319f25bde55be2c0
Snazzah <suggesttosnazzy@gmail.com>
2018-02-14 00:31:04 -0600

    fix buttons and core plugin css

--- elements/SettingsOptionButton.js
+++ elements/SettingsOptionButton.js
@@ -4,19 +4,11 @@ const e = React.createElement
 
 class SettingsButton extends React.Component {
   get brandClass () {
-    return 'flexChild-1KGW5q buttonBrandFilledDefault-2Rs6u5 buttonFilledDefault-AELjWf buttonDefault-2OLW-v button-2t3of8 buttonFilled-29g7b5 buttonBrandFilled-3Mv0Ra'
+    return 'button-2t3of8 lookFilled-luDKDo colorBrand-3PmwCE sizeSmall-3g6RX8 grow-25YQ8u'
   }
 
   get outlineClass () {
-    return 'flexChild-1KGW5q buttonRedOutlinedDefault-1VCgwL buttonOutlinedDefault-3FNQnZ buttonDefault-2OLW-v button-2t3of8 buttonOutlined-38aLSW buttonRedOutlined-2t9fm_'
-  }
-
-  get brandContentsClass () {
-    return 'contentsDefault-nt2Ym5 contents-4L4hQM contentsFilled-3M8HCx contents-4L4hQM'
-  }
-
-  get outlineContentsClass () {
-    return 'contentsDefault-nt2Ym5 contents-4L4hQM contentsOutlined-mJF6nQ contents-4L4hQM'
+    return 'button-2t3of8 lookOutlined-1c5nhl colorRed-3HTNPV sizeSmall-3g6RX8 grow-25YQ8u'
   }
 
   render () {
@@ -33,9 +25,7 @@ class SettingsButton extends React.Component {
       e(
         'div',
         {
-          className: this.props.outline
-            ? this.outlineContentsClass
-            : this.brandContentsClass
+          className: 'contents-4L4hQM'
         },
         this.props.text
       )
