42810058b6a6225e778a21c8bcc0efc6203c95cb
Snazzah <suggesttosnazzy@gmail.com>
2018-02-18 04:51:18 -0600

    no colors

--- engine/plugins/plugins/components/ListEntry.jsx
+++ engine/plugins/plugins/components/ListEntry.jsx
@@ -65,7 +65,7 @@ module.exports = class ListEntry extends PureComponent {
             defaultValue={entry.loaded}
           />
           <div class='DI-plugin-meta'>
-            <strong style={{ backgroundImage: `url(${entry.icon || 'https://discordinjections.xyz/img/logo-alt-nobg.svg'}`, color: `#${entry.color}` }}>
+            <strong style={{ backgroundImage: `url(${entry.icon || 'https://discordinjections.xyz/img/logo-alt-nobg.svg'}` }}>
               {entry.package.name}
             </strong>
 
