40cc9cbbca8b80bc0250852877a5c76ffb6d8a08
Ch. König <criss@paars.de>
2018-05-05 20:46:08 +0200

    add badges to messages-wrapper nodes too

--- engine/plugins/contrib/index.js
+++ engine/plugins/contrib/index.js
@@ -19,46 +19,52 @@ module.exports = class hooks extends Plugin {
         return
       }
 
-      if (!node.classList.contains('message-group')) {
-        return
-      }
-
-      const rInst = this.r.getReactInstance(node)
-      const avatar = rInst.memoizedProps
-        ? rInst.memoizedProps.children[0]
-        : rInst.props.children[0]
-
-      let user = null
-      try {
-        const avatarProps = avatar.props.children
-
-        user = avatarProps.memoizedProps
-          ? avatarProps.memoizedrops.user
-          : avatarProps.props.user
-      } catch (err) {
-        this.error('failed to fetch the user', user, err)
-        return
-      }
-
-      const name = this.DI.contributors[user.id]
-      if (!name) {
-        return
-      }
-
-      const nametag = node.querySelector('.username-wrapper')
-
-      if (nametag) {
-        nametag.appendChild(
-          this.r.createElement(
-            `<div class="DI-contrib">
-                <div class="tooltip tooltip-top tooltip-black">DI Contributor ${name}</div>
-              </div>`
-          )
+      if (node.classList.contains('messages-wrapper')) {
+        Array.from(node.querySelectorAll('.message-group')).forEach(node =>
+          this.applyBadge(node)
         )
-      } else {
-        this.warn('failed to apply nametag badge', user, name, nametag)
+      } else if (node.classList.contains('message-group')) {
+        this.applyBadge(node)
       }
     })
+  }
+
+  applyBadge (node) {
+    const rInst = this.r.getReactInstance(node)
+    const avatar = rInst.memoizedProps
+      ? rInst.memoizedProps.children[0]
+      : rInst.props.children[0]
+
+    let user = null
+    try {
+      const avatarProps = avatar.props.children
+
+      user = avatarProps.memoizedProps
+        ? avatarProps.memoizedrops.user
+        : avatarProps.props.user
+    } catch (err) {
+      this.error('failed to fetch the user', user, err)
+      return
+    }
+
+    const name = this.DI.contributors[user.id]
+    if (!name) {
+      return
+    }
+
+    const nametag = node.querySelector('.username-wrapper')
+
+    if (nametag) {
+      nametag.appendChild(
+        this.r.createElement(
+          `<div class="DI-contrib">
+                <div class="tooltip tooltip-top tooltip-black">DI Contributor ${name}</div>
+              </div>`
+        )
+      )
+    } else {
+      this.warn('failed to apply nametag badge', user, name, nametag)
+    }
   }
 
   get iconURL () {
