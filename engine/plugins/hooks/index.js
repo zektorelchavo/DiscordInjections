cae8137abdadc602d015a7a5dd6d76dc627b2cab
Ch. König <criss@paars.de>
2018-09-20 19:02:04 +0200

    rewrite hooks

--- engine/plugins/hooks/index.js
+++ engine/plugins/hooks/index.js
@@ -1,22 +1,37 @@
 const { Plugin } = require('elements')
 const Promise = require('bluebird')
+const { superstruct } = require('superstruct')
+
+const struct = superstruct({
+  types: {
+    function: v => typeof v === 'function',
+    class: v => typeof v === 'function' && v.toString().match(/^class\s*{/i)
+  }
+})
 
 module.exports = class hooks extends Plugin {
-  get messageHandler () {
-    return this.fetchModule(require('./messageHandler.json'))
-  }
-
   get guildHandler () {
     return this.fetchModule(require('./guildHandler.json'))
   }
 
+  get loadedModules () {
+    if (!this._webpackRequire) {
+      return 0
+    }
+
+    return Object.keys(this._webpackRequire.c).length
+  }
+
+  get modules () {
+    if (!this._webpackRequire) {
+      return {}
+    }
+
+    return this._webpackRequire.c
+  }
+
   async preload () {
-    this._hooks = new Map()
-
-    this._require = null
-    this.webPackLoadQueue = []
-
-    this.messageHandler.then(handler => {
+    /*    this.messageHandler.then(handler => {
       Object.keys(handler).forEach(k => {
         this.debug(`hiding ${k}`)
         handler['$$' + k] = handler[k]
@@ -26,115 +41,198 @@ module.exports = class hooks extends Plugin {
         }
       })
     })
-
-    this._moduleList = {}
-    this._indexed = -1
+    */
+    this._fetchWebpackRequire()
   }
 
-  // WebPackLoad
-  async webPackLoad (cb, name = Math.random().toString()) {
+  async load () {
+    this.debug('Getting environment')
+    this.environment = await this.fetchModule(
+      m => m.Endpoints && m.DownloadLinks
+    )
+
+    this.debug('Getting globals')
+    this.globals = await this.fetchModule(
+      m =>
+        m.copy &&
+        m.cut &&
+        m.paste &&
+        m.platform &&
+        m.architecture &&
+        m.releaseChannel
+    )
+
+    this.debug('Getting restClient')
+    this.restClient = await this.fetchModule(
+      m => m.delete && m.get && m.patch && m.post && m.put && m.getAPIBaseURL
+    )
+
+    this.debug('Getting history')
+    this.history = await this.fetchModule(
+      m => m.getFingerprintLocation && m.transitionToGuild && m.transitionTo
+    )
+
+    this.debug('Getting userAgent')
+    this.userAgent = await this.fetchModule(
+      m => m.description && m.layout && m.ua && m.version
+    )
+
+    this.debug('Getting permissionManager')
+    this.permissionManager = await this.fetchModule(
+      m => m.isRoleHigher && m.can && m.canEveryone && m.areChannelsLocked
+    )
+
+    this.debug('Getting assets')
+    this.assets = await this.fetchModule(
+      m =>
+        m.hasAnimatedAvatar &&
+        m.getEmojiURL &&
+        m.getChannelIconURL &&
+        m.getGuildSplashURL
+    )
+
+    this.debug('Getting activityManager')
+    this.activityManager = await this.fetchModule(
+      m =>
+        m.canJoinOrSpectate &&
+        m.hasRichActivity &&
+        m.isGameActivity &&
+        m.isOnXbox &&
+        m.isStreaming
+    )
+
+    this.debug('Getting gameManager')
+    this.gameManager = await this.fetchModule(
+      m => m.addGame && m.editName && m.getGameNews && m.getGames && m.launch
+    )
+
+    this.debug('Getting messageHandler')
+    this.messageHandler = await this.fetchModule(
+      m =>
+        m.receiveMessage &&
+        m.updateEditMessage &&
+        m.fetchMessages &&
+        m.sendBotMessage
+    )
+
+    this.debug('Getting groupDM')
+    this.groupDM = await this.fetchModule(
+      m =>
+        m.addRecipient &&
+        m.closePrivateChannel &&
+        m.openPrivateChannel &&
+        m.convertToGuild &&
+        m.setName &&
+        m.setIcon
+    )
+
+    this.debug('Getting channelManager')
+    this.channelManager = await this.fetchModule(
+      m => m.selectChannel && m.selectPrivateChannel
+    )
+
+    this.debug('Getting spotifyActivity')
+    this.spotifyActivity = await this.fetchModule(m => m.isSpotifyParty)
+
+    this.debug('Getting user')
+    this.user = await this.fetchModule(
+      m =>
+        m.createGuild &&
+        m.createRole &&
+        m.nsfwAgree &&
+        m.nsfwDisagree &&
+        m.selectGuild
+    )
+
+    this.debug('Getting messageManager')
+    this.messageManager = await this.fetchModule(
+      m => m.createBotMessage && m.isMentioned
+    )
+
+    this.debug('Getting invites')
+    this.invites = await this.fetchModule(m => m.findInvite && m.resolveInvite)
+
+    this.debug('Getting caller')
+    this.caller = await this.fetchModule(m => m.call && m.doRing && m.ring)
+
+    this.debug('Getting spotify')
+    this.spotify = await this.fetchModule(
+      m => m.play && m.fetchIsSpotifyProtocolRegistered
+    )
+
+    this.debug('Getting voiceManager')
+    this.voiceManager = await this.fetchModule(
+      m => m.enable && m.setOutputDevice && m.toggleSelfMute
+    )
+
+    this.debug('Getting inviteManager')
+    this.inviteManager = await this.fetchModule(
+      m => m.acceptInvite && m.resolveInvite
+    )
+    /*
+      Unkown:
+        - Last known ID: 106
+          deduplicate, getVoiceSettings, hasMessageReadPermission, transformUser, validateSocketClient
+
+        - Last known ID: 222
+          can, canManageUser, permissionStoreDidChange
+    */
+  }
+
+  async _fetchWebpackRequire () {
     while (!window.webpackJsonp) {
       await Promise.delay(1)
     }
 
-    // newstyle webpack
-    if (window.webpackJsonp.push) {
-      // overload undefined
-      if (!window.webpackJsonp._push) {
-        window.webpackJsonp._push = window.webpackJsonp.push
+    const name = Math.random().toString()
+    this._webpackRequire = window.webpackJsonp.push([
+      [],
+      { [name]: (m, e, r) => (m.exports = r) },
+      [[name]]
+    ])
 
-        // overwrite webpack jsonp push
-        window.webpackJsonp.push = jsonp => {
-          // is _require unset?
-          if (this._require === null) {
-            // grab the first element in the module cache
-            const callID = Object.keys(jsonp[1])[0]
-            const call = jsonp[1][callID]
-
-            // attach custom logic to module
-            jsonp[1][callID] = (_module, _exports, _require) => {
-              this.debug('stealing require reference')
-              this._require = _require
-              this.processWPLQueue()
-
-              return call(_module, _exports, _require)
-            }
-          }
-          return window.webpackJsonp._push(jsonp)
-        } // fn push
-      } // !_push
-
-      return this.processWPLQueue(cb)
-    } else {
-      return window.webpackJsonp([name], { [name]: cb }, [name])
-    }
+    delete this._webpackRequire.m[name]
+    delete this._webpackRequire.c[name]
   }
 
-  processWPLQueue (newCallback = null) {
-    if (newCallback) {
-      this.debug('Adding new hook callback')
-      this.webPackLoadQueue.push(newCallback)
-    }
+  async fetchModule (cb) {
+    return new Promise(async (resolve, reject) => {
+      while (!this._webpackRequire) {
+        await Promise.delay(1)
+      }
 
-    if (this._require) {
-      // dont lock up current callback pls :)
-      setImmediate(() => {
-        this.debug('draining hook callbacks')
-        while (this.webPackLoadQueue.length > 0) {
-          const cb = this.webPackLoadQueue.shift()
-          cb(null, {}, this._require)
-        }
-      })
-    }
-  }
+      let unsuccessfulIterations = 0
+      let lastLength = 0
+      let idx = 0
 
-  async fetchModule (def) {
-    if (this._hooks.has(def.name)) {
-      return this._hooks.get(def.name)
-    }
+      while (unsuccessfulIterations < 5) {
+        if (lastLength < this.loadedModules) {
+          const keys = Object.keys(this.modules)
 
-    return new Promise(resolve => {
-      let modIdx = 0
-      const findModule = () =>
-        this.webPackLoad((_module, _exports, _require) => {
-          this.debug('Searching module...', def.name, modIdx)
-          for (modIdx; modIdx < Object.values(_require.c).length; modIdx++) {
-            // ignore failed modules
-            const modDefinition = _require.c[modIdx]
-            if (!modDefinition || !modDefinition.exports) {
+          for (idx; idx < this.loadedModules; idx++) {
+            let mod = this.modules[keys[idx]]
+            if (!mod || !mod.exports) {
               continue
             }
 
-            // we are only interested in the exports, ignore the metadata
-            const mod = modDefinition.exports
-
-            if (
-              def.hooks.every(endpoint => {
-                let node = mod[endpoint.name]
-                if (!node && mod.default) {
-                  node = mod.default[endpoint.name]
-                }
-                if (!node) {
-                  return false
-                }
-
-                if (endpoint.type) {
-                  return typeof node === endpoint.type
-                }
-
-                return true
-              })
-            ) {
-              this.debug('Found module!', def.name)
-              this._hooks.set(def.name, mod)
+            mod = mod.exports
+            if (cb(mod)) {
               return resolve(mod)
+            } else if (mod.default && cb(mod.default)) {
+              return resolve(mod.default)
             }
           }
+        } else {
+          unsuccessfulIterations++
 
-          this.debug('...fill buffer', def.name)
-          return Promise.delay(1).then(findModule)
-        })
-      return findModule()
+          // force other coroutines to process first
+          await Promise.delay(1)
+        }
+
+        lastLength = this.loadedModules
+      }
+
+      reject(new Error('module not found..!'))
     })
   }
 
